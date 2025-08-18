4379e9a3c972e3785e8639de6aa59c61801c7b0e
ZakirTqvi <husainitaqvi4@gmail.com>
2025-08-18 23:22:21 +0530

    files added

--- backend/server.js
+++ backend/server.js
@@ -1,56 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "../models/product.model.js";
+
+import productRoutes from "./routes/product.route.js";
+
 
 dotenv.config();
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.get("/api/products", async (req, res) => {
-
-  try{
-     const product = await Product.find({});
-     res.status(200).json({ success: true, data: product})
-  } catch (error) {
-    res.status(404).json( { success: false, message: "Error in fetching data"})
-  }
-})
-
-app.post("/api/products", async (req, res) => {
-  const product = req.body; // user will send this data
-
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, massage: "Please provide all fields" });
-  }
-
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error("Error in Create product:", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.delete("/api/products/:id", async (req, res) =>{
-  const {id} = req.params;
-try{
- await Product.findByIdAndDelete(id);
- res.status(200).json({ success: true, message : "Product deleted" })
-}catch(error){
-  res.status(404).json({ success:false, message: "Product not found"})
-}
-});
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
-  console.log("Server is listening at http://localhost:500");
+  console.log("Server is listening at http://localhost:" + PORT);
 });
