54d333b8a17b2c9fda8e361faae76df0d35ae887
ZakirTqvi <husainitaqvi4@gmail.com>
2025-08-31 16:50:51 +0530

    updated

---
+++ frontend/src/store/product.js
@@ -0,0 +1,37 @@
+import {create} from "zustand"
+
+export const useProductStore = create((set) => ({
+    products: [],
+    setProducts: (products) => set({ products }),
+    createProduct: async (newProduct) => {
+        if(!newProduct.name || !newProduct.image || !newProduct.price) {
+            return {success:false, message:"Please fill in all fileds."}
+        }
+        const res = await fetch("/api/products", {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(newProduct)
+        })
+        const data = await res.json();
+        set((state) => ({products:[...state.products, data.data]}));
+         return {success:true, message:"Product created successfully"}
+    },
+    fetchProducts: async () =>{
+        const res = await fetch("/api/products");
+        const data = await res.json();
+        set({ products: data.data});
+    },
+    deleteProduct: async (pid) => {
+        const res = await fetch(`/api/products/${pid}`, {
+            method: "DELETE",
+        });
+        const data = await res.json();
+        if(!data.success) return { success: false, message: data.message};
+
+        // update the ui immediately, without needing to refetch
+        set(state => ({ products: state.products.filter(product => product._id !== pid) }));
+        return { success: true, message: data.message};
+    }
+}));
