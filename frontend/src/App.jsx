a4d4def0fb78a22ce3d365d7c9e9375aa68ae2f7
ZakirTqvi <husainitaqvi4@gmail.com>
2025-08-31 16:51:45 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
-import Navbar from "./componetns/Navbar"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,56 +1,100 @@
-import { Box, Button, Container, Heading, useColorModeValue, VStack, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  useColorModeValue,
+  VStack,
+  Input,
+  useToast} from "@chakra-ui/react";
 import { useState } from "react";
+import { useProductStore } from "../store/product";
+import { set } from "mongoose";
 const CreatePage = () => {
-
   const [newProduct, setNewProduct] = useState({
     name: "",
     price: "",
     image: "",
   });
 
-  const handleAddProduct = () => {
-    console.log(newProduct);
+  const toasts = useToast();
+  const { createProduct } = useProductStore();
+
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct);
+    if(!success) {
+      toasts({
+        title: "Error",
+        description: message,
+        status: "error",
+        isClosable: true,
+      });
+    } else {
+      toasts({
+        title: "Success",
+        description: message,
+        status: "success",
+        isClosable: true,
+      });
+    }
+
+    setNewProduct({
+      name: "",
+      price: "",
+      image: "",
+    });
+    
   };
 
-  return( <Container maxW={"container.sm"}>
-  <VStack
-   spacing={4}
-  >
-   <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
-     Create New Product
-   </Heading>
-
-   <Box
-    w={"full"} bg={useColorModeValue("white", "gray.800")} 
-    p={6} rounded={"lg"} shadow={"md"}>
+  return (
+    <Container maxW={"container.sm"}>
       <VStack spacing={4}>
-       <Input
-        placeholder='Product Name'
-        name='name'
-        value={newProduct.name}
-        onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
-       />
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
 
-        <Input
-        placeholder='Price'
-        name='price'
-        value={newProduct.price}
-        onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
-       />
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
 
-        <Input
-        placeholder='Image URL'
-        name='image'
-        value={newProduct.image}
-        onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
-       />
+            <Input
+              placeholder="Price"
+              name="price"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+
+            <Input
+              placeholder="Image URL"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+          </VStack>
+          <Button colorScheme="blue" onClick={handleAddProduct} w="full">
+            Add Product
+          </Button>
+        </Box>
       </VStack>
-<Button colorScheme="blue" onClick={handleAddProduct} w='full'>
-  Add Product
-  </Button>
-   </Box>
-  </VStack>
-  </Container>
-)};
+    </Container>
+  );
+};
 
-export default CreatePage
+export default CreatePage;
